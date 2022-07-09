959ac47a25c85c0fb381469f562712f32b2da7d1
unknown <themoontimer@gmail.com>
2022-07-09 09:17:34 -0500

    Modified
    index.html, styles.css modified

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
 const clickBtn = document.querySelector("#click");
 const showNum = document.querySelector("#plus");
 
 clickBtn.addEventListener('click', ()=> {
     showNum.textContent++;
 });
->>>>>>> 0dcdcddab220a7ef85efe9c034feac7d79efd1d5
