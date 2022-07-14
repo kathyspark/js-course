6280bcddf65e4918b13d427e97bfc514413f0245
The Moon Timer <71471331+FreeCarrier@users.noreply.github.com>
2022-07-14 17:18:17 -0500

    empty form added

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
+const inputs = document.getElementsByTagName("input");
 const right = document.querySelector(".right");
 const addBtn = document.querySelector("#add-btn");
 
 addBtn.addEventListener('click', addElement);
 
 function addElement() {
-    const newRow = document.createElement("div");
-    newRow.setAttribute("class", "row");
-    for(let i = 0; i < 5; i++) {
-        const rowItem = document.createElement("span");
-        rowItem.setAttribute("class", "item cols");
-        rowItem.textContent = document.getElementsByTagName("input")[i].value;
-        newRow.appendChild(rowItem);
-    }
+    if(inputs[0].value === "" && inputs[1].value === "" && inputs[2].value === "" && inputs[3].value === "" && inputs[4].value === "") {
+        alert("Oops! You haven't filled the form yet D:");
+    } else {
+        const newRow = document.createElement("div");
+        newRow.setAttribute("class", "row");
+        for(let i = 0; i < 5; i++) {
+            const rowItem = document.createElement("span");
+            rowItem.setAttribute("class", "item cols");
+            rowItem.textContent = document.getElementsByTagName("input")[i].value;
+            newRow.appendChild(rowItem);
+        }
 
-    right.appendChild(newRow);
-    for(let i = 0; i < 5; i++) {
-        document.getElementsByTagName("input")[i].value = '';
+        right.appendChild(newRow);
+        for(let i = 0; i < 5; i++) {
+            inputs[i].value = '';
+        }
     }
 }
