71f2d81a55122430c5d745fb2bf0e9a67c0ad913
The Moon Timer <71471331+FreeCarrier@users.noreply.github.com>
2022-07-13 19:13:51 -0500

    js code updated

--- main.js
+++ main.js
@@ -6,29 +6,22 @@ const inputEmail = document.querySelector("#input-email");
 const inputId = document.querySelector("#input-id");
 const addBtn = document.querySelector("#add-btn");
 
-addBtn.addEventListener('click', crearElemento);
+addBtn.addEventListener('click', addElement);
 
-function crearElemento() {
-    const row = document.createElement("div");
-    row.setAttribute("class", "rows");
-    for(let i = 0; i <= 4; i++) {
-        const cols = document.createElement("span");
-        cols.setAttribute("class", "item cols");
-        row.appendChild(cols);
-        let colItems = document.querySelectorAll('.item');
-        let counter = colItems.length - 1;
-        console.log(counter);
-        colItems[counter--].textContent = inputId.value;
-        colItems[counter--].textContent = inputEmail.value;
-        colItems[counter--].textContent = inputAge.value;
-        colItems[counter--].textContent = inputLastName.value;
-        colItems[counter].textContent = inputName.value;
+function addElement() {
+    console.log(document.getElementsByTagName("input")[0].value);
+    const newRow = document.createElement("div");
+    newRow.setAttribute("class", "row");
+    for(let i = 0; i < 5; i++) {
+        const rowItem = document.createElement("span");
+        rowItem.setAttribute("class", "item cols");
+        rowItem.textContent = document.getElementsByTagName("input")[i].value;
+        newRow.appendChild(rowItem);
     }
+    console.log(newRow);
 
-    inputId.value = '';
-    inputEmail.value = '';
-    inputAge.value = '';
-    inputLastName.value = '';
-    inputName.value = '';
-    right.appendChild(row);
+    right.appendChild(newRow);
+    for(let i = 0; i < 5; i++) {
+        document.getElementsByTagName("input")[i].value = '';
+    }
 }
