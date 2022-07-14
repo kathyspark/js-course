ee65a3f5671f497a439d89d6764569027d74818e
The Moon Timer <71471331+FreeCarrier@users.noreply.github.com>
2022-07-14 17:25:26 -0500

    alert message

--- main.js
+++ main.js
@@ -6,20 +6,22 @@ addBtn.addEventListener('click', addElement);
 
 function addElement() {
     if(inputs[0].value === "" && inputs[1].value === "" && inputs[2].value === "" && inputs[3].value === "" && inputs[4].value === "") {
-        alert("Oops! You haven't filled the form yet D:");
+        alert("Oops! You must enter the data first");
     } else {
         const newRow = document.createElement("div");
         newRow.setAttribute("class", "row");
+
         for(let i = 0; i < 5; i++) {
             const rowItem = document.createElement("span");
             rowItem.setAttribute("class", "item cols");
             rowItem.textContent = document.getElementsByTagName("input")[i].value;
             newRow.appendChild(rowItem);
         }
-
         right.appendChild(newRow);
+        
+        // This empties the form inputs
         for(let i = 0; i < 5; i++) {
-            inputs[i].value = '';
+            inputs[i].value = "";
         }
     }
 }
